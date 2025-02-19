cf8fea194227069f6e14a435f506912c4bb612b9
Kevin O'Malley <omallek@gmail.com>
2025-02-19 09:56:21 -0500

    Update layout html style and convert to rem units

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,23 +4,6 @@
 	let { children } = $props();
 </script>
 
-<div class="layout background-gradient">
-	<Header />
-	{@render children()}
-	<Footer />
-</div>
-
-<style>
-	.layout {
-		width: 100%;
-		height: 100%;
-	}
-	.background-gradient {
-		background: radial-gradient(
-			circle at center,
-			hsla(222, 24%, 30%, 0.5) 0%,
-			hsla(222, 0%, 0%, 0) 50%,
-			hsla(222, 0%, 0%, 0) 100%
-		);
-	}
-</style>
+<Header />
+{@render children()}
+<Footer />
